274133c77eb7a83dae9886e3c5e1183179877427
ben-wycliff <ben12wycliff@gmail.com>
2020-05-21 14:50:45 +0300

    Adds CORS

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ from flask import Flask, jsonify,request
 # from resources.admin import admin
 import face_recognition, pickle
 import numpy as np
+from flask_cors import CORS
+# from binascii import a2b_base64
+from PIL import Image
+from io import BytesIO
+import base64
+
 app = Flask(__name__)
+CORS(app)
 
 # app.config["MONGODB_SETTINGS"] = {"host": ""}  # database url
 
@@ -15,6 +22,11 @@ app = Flask(__name__)
 @app.route('/detect', methods=["POST"])
 def face_detect():
     body = request.get_json()
+    datauri = body['datauri']
+    # im = Image.open(BytesIO(base64.b64decode(datauri + b'===')))
+    # with open('img.jpg', 'wb') as f:
+    #     f.write(image_data)
+    # im.save('image.png', 'PNG')
     print(body)
     image = face_recognition.load_image_file('./resources/image.jpg')
     # with open('./resources/classifier.pkl', 'rb') as file:

a92577ee247c249bf18c2ca44ec8ddf83404f8b7
ben-wycliff <ben12wycliff@gmail.com>
2020-05-23 15:39:10 +0300

    receives image and checks the class that matches it

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ from flask import Flask, jsonify,request
 import face_recognition, pickle
 import numpy as np
 from flask_cors import CORS
-# from binascii import a2b_base64
-from PIL import Image
-from io import BytesIO
+
 import base64
 
 app = Flask(__name__)
@@ -20,28 +18,30 @@ CORS(app)
 
 
 @app.route('/detect', methods=["POST"])
+# @cross_origin()
 def face_detect():
-    body = request.get_json()
-    datauri = body['datauri']
-    # im = Image.open(BytesIO(base64.b64decode(datauri + b'===')))
-    # with open('img.jpg', 'wb') as f:
-    #     f.write(image_data)
-    # im.save('image.png', 'PNG')
-    print(body)
-    image = face_recognition.load_image_file('./resources/image.jpg')
-    # with open('./resources/classifier.pkl', 'rb') as file:
-        # model = pickle.load(file)
-    model = pickle.load(open('./resources/classifier.sav', 'rb'))
-    face_location = face_recognition.face_locations(image, number_of_times_to_upsample=2)
-    face_encodings = face_recognition.face_encodings(image, known_face_locations=face_location)
-    if len(face_encodings) != 1:
-        return {"user": "Unknown"}
-    else:
-        encodings_array = np.array([face_encodings[0]])
-        prediction = model.predict(encodings_array)
-        # prediction = model.predict([face_encodings])
-        print(prediction)
-        return {"user": prediction[0]}, 200
-
+    try:
+        body = request.get_json()
+        datauri = body['datauri']
+        datauri = datauri.partition(',')[2]
+        img = open('testimage.png', 'wb')
+        img.write(base64.b64decode(datauri))
+        img.close()
+        image = face_recognition.load_image_file('testimage.png')
+        # with open('./resources/classifier.pkl', 'rb') as file:
+            # model = pickle.load(file)
+        model = pickle.load(open('model/classifier.sav', 'rb'))
+        face_location = face_recognition.face_locations(image, number_of_times_to_upsample=2)
+        face_encodings = face_recognition.face_encodings(image, known_face_locations=face_location)
+        if len(face_encodings) != 1:
+            return {"user": "Unknown"}
+        else:
+            encodings_array = np.array([face_encodings[0]])
+            prediction = model.predict(encodings_array)
+            # prediction = model.predict([face_encodings])
+            print(prediction)
+            return {"user": prediction[0]}, 200
+    except Exception:
+        return "internal server error", 500
 
 app.run(debug=True)

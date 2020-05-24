f57a343e2c449965f514ca4935e24f106f0dc7ab
ben-wycliff <ben12wycliff@gmail.com>
2020-05-24 21:22:10 +0300

    creates deploy file

---
+++ deploy.py
@@ -0,0 +1,51 @@
+from flask import Flask, jsonify,request
+# from database.db import initialize_db
+# from resources.admin import admin
+import face_recognition, pickle
+import numpy as np
+import pandas as pd
+from flask_cors import CORS
+
+import base64
+
+app = Flask(__name__)
+CORS(app)
+
+df = pd.read_csv('./data/train-face-encodings.csv')
+df.groupby("name").mean()
+
+
+@app.route('/')
+def index():
+    return '<h1>Home</h1>'
+
+
+@app.route('/detect', methods=["POST"])
+def face_detect():
+    # try:
+        body = request.get_json()
+        datauri = body['datauri']
+        datauri = datauri.partition(',')[2]
+        img = open('testimage.png', 'wb')
+        img.write(base64.b64decode(datauri))
+        img.close()
+        image = face_recognition.load_image_file('testimage.png')
+
+        model = pickle.load(open('model/classifier.sav', 'rb'))
+        face_location = face_recognition.face_locations(image, number_of_times_to_upsample=2)
+        face_encodings = face_recognition.face_encodings(image, known_face_locations=face_location)
+        if len(face_encodings) != 1:
+            return {"user": "Unknown"}
+        else:
+            encodings_array = np.array([face_encodings[0]])
+            prediction = model.predict(encodings_array)
+            # prediction = model.predict([face_encodings])
+            df = pd.read_csv('./data/train-face-encodings.csv')
+            avg = df.groupby("name").mean()
+            known = list(avg.loc[prediction[0]])
+            known = known[-128: ]
+            print(prediction)
+            compare = face_recognition.compare_faces([known], face_encodings[0], tolerance=0.4)[0]
+            if compare:
+                return {"user": prediction[0]}, 200
+            return {"user": "Unknown"}

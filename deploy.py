6b9967b2cc27357328d6ea54a1e60333c12de702
ben-wycliff <benclifg@gmail.com>
2020-10-16 17:56:15 +0300

    Adds prediction end point

--- deploy.py
+++ deploy.py
@@ -1,28 +1,34 @@
 from flask import Flask, jsonify,request
 # from database.db import initialize_db
 # from resources.admin import admin
-from .utils import generate_embeddings
-from .utils import detect_face
+from utils import generate_embeddings
+from utils import detect_face
 import face_recognition, pickle
 import numpy as np
 import pandas as pd
 from flask_cors import CORS
+import tensorflow as tf
+tf.get_logger().setLevel('ERROR')
 
 import base64
 
 app = Flask(__name__)
 CORS(app)
 
+# app.config["MONGODB_SETTINGS"] = {"host": ""}  # database url
+
+# initialize_db(app)
+
+# app.register_blueprint(admin)
 df = pd.read_csv('./data/train-face-encodings.csv')
 df.groupby("name").mean()
-
 
 @app.route('/')
 def index():
     return '<h1>Home</h1>'
 
-
 @app.route('/detect', methods=["POST"])
+# @cross_origin()
 def face_detect():
     # try:
         body = request.get_json()
@@ -32,7 +38,8 @@ def face_detect():
         img.write(base64.b64decode(datauri))
         img.close()
         image = face_recognition.load_image_file('testimage.png')
-
+        # with open('./resources/classifier.pkl', 'rb') as file:
+            # model = pickle.load(file)
         model = pickle.load(open('model/classifier.sav', 'rb'))
         face_location = face_recognition.face_locations(image, number_of_times_to_upsample=2)
         face_encodings = face_recognition.face_encodings(image, known_face_locations=face_location)
@@ -51,19 +58,25 @@ def face_detect():
             if compare:
                 return {"user": prediction[0]}, 200
             return {"user": "Unknown"}
+    # except Exception:
+    #     print(Exception)
+    #     return "internal server error", 500
 
-@app.route('/login', methods=["POST"])
+@app.route('/predict', methods=["POST"])
 def face_recognition():
-    body = request.get_json()
-    datauri = body['datauri']
-    datauri = datauri.partition(',')[2]
-    img = open('userface.png', 'wb')
-    img.write(base64.b64decode(datauri))
-    img.close()
-    face = detect_face('./userface.png')
-    face_embeddings = generate_embeddings(face)
-    # importing facenet-classifier
-    classifier = pickle.load(open('models/classifier-facenet.sav', 'rb'))
-    prediction = classifier.predict(face_embeddings)
-    print(prediction)
-    return {"prediction": prediction}
+    try:
+        body = request.get_json()
+        datauri = body['datauri']
+        datauri = datauri.partition(',')[2]
+        img = open('userface.png', 'wb')
+        img.write(base64.b64decode(datauri))
+        img.close()
+        face = detect_face.detect_face('./userface.png')
+        face_embeddings = generate_embeddings.generate_embeddings(face)
+        # importing facenet-classifier
+        classifier = pickle.load(open('models/classifier-facenet.sav', 'rb'))
+        prediction = classifier.predict(face_embeddings)
+        print(prediction)
+        return {"prediction": str(prediction[0])}
+    except:
+        return {"message": "Something went wrong at the backend"}
